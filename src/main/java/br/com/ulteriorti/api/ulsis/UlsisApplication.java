93ce8f2cdb1113e13674baffe8adb01050edb487
Denis Souza <denis.souza@ulteriorti.com.br>
2023-01-21 20:43:28 -0300

    Commit Inicial

---
+++ src/main/java/br/com/ulteriorti/api/ulsis/UlsisApplication.java
@@ -0,0 +1,13 @@
+package br.com.ulteriorti.api.ulsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlsisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ulteriorti/api/ulsis/UlsisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ulteriorti.api.ulsis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlsisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
